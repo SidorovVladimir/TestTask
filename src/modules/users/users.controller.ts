84ae4abcec0faf7ec820b7f01759cef678807b9a
Sidorov Vladimir <v.sidorov29091988@gmail.com>
2024-07-18 22:07:51 +0700

    Create method create user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+
+    @Post('create')
+    create(@Body() dto: CreateUserDTO): Promise<CreateUserDTO> {
+        return this.usersService.create(dto);
+    }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './models/user.models';
+import { CreateUserDTO } from './dto';
 
 @Injectable()
 export class UsersService {
@@ -8,6 +9,18 @@ export class UsersService {
         @InjectModel(User)
         private readonly userRepository: typeof User,
     ) {}
+
+    async create(dto: CreateUserDTO): Promise<CreateUserDTO> {
+
+        await this.userRepository.create({
+            firstname: dto.firstname,
+            username: dto.username,
+            password: dto.password,
+            email: dto.email,
+        })
+        return dto;
+        
+    }
 
 
     
